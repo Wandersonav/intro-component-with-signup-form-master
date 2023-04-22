8c9400be272fbc4384b9fc03158e4b902a6d6ee1
wandersonav <wandersonav07@gmail.com>
2023-04-22 18:32:39 -0300

    Atualização 22/04

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const formulario = document.getElementsByClassName("formulario");
+const enviar = document.getElementById("botao");
+const descricao = document.getElementById("descricao");
+
+enviar.addEventListener("click",(even) => {
+
+console.log(even.target.form.id = descricao[0]);
+console.log(even.target.form);
+//console.log(even.target.form.id = descricao[1])
+//console.log(even.target.form.id = descricao[2])
+//console.log(even.target.form.id = descricao[3])
+
+//if(even.target.form.id = descricao[0] == '') {descricao.innerHTML = "vazio"}
+
+//even.target.form.id = descricao[1])
+//even.target.form.id = descricao[2])
+//even.target.form.id = descricao[3])
+
+//even.target.form
+
+})

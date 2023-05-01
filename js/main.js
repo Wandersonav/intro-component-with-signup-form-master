7bbe8f624981d1bcc2454119d05f7c0606f058c1
wandersonav <wandersonav07@gmail.com>
2023-05-01 11:02:47 -0300

    Encerrando projeto

--- js/main.js
+++ js/main.js
@@ -1,31 +1,42 @@
 const formulario = document.getElementsByClassName("formulario");
 const enviar = document.getElementById("botao");
 const descricao = document.querySelectorAll(".descricao");
+const input = document.querySelector('input[name="name"]');
 
 enviar.addEventListener("click",(even) => {
-
+   // let el = document.querySelector('.control');
 
 nome = even.target.form[0].value
 ultimoNome = even.target.form[1].value
 email = even.target.form[2].value
 Password = even.target.form[3].value
 
+let btn = document.getElementById("control1");
+let btn2 = document.getElementById("control2");
+let btn3 = document.getElementById("control3");
+let btn4 = document.getElementById("control4");
 
-if (nome === ''){descricao1.innerHTML = "First Name cannot be empty"}else{descricao1.innerHTML = ""};
-if (ultimoNome === ''){descricao2.innerHTML = "Last Name cannot be empty"}else{descricao2.innerHTML = ""};
-if (email === ''){descricao3.innerHTML = "Looks like this is not an email"}else{descricao3.innerHTML = ""};
-if (Password === ''){descricao4.innerHTML = "Password cannot be empty"}else{descricao4.innerHTML = ""};
+if (nome === ''){descricao1.innerHTML = "First Name cannot be empty",btn.classList.add("erro")}else{descricao1.innerHTML = ""};
+if (ultimoNome === ''){descricao2.innerHTML = "Last Name cannot be empty",btn2.classList.add("erro")}else{descricao2.innerHTML = ""};
+if (email === ''){descricao3.innerHTML = "Looks like this is not an email",btn3.classList.add("erro")}else{descricao3.innerHTML = ""};
+if (Password === ''){descricao4.innerHTML = "Password cannot be empty",btn4.classList.add("erro")}else{descricao4.innerHTML = ""};
+
+
+
+
+
+
 
 
 })
 
+enviar.addEventListener('submit',(e) =>{
+e.preventDefault()
+console.log("teste")
+//formulario.classList.add('teste')})
 
 
-
-
-
-
-
+})
 
 
 
